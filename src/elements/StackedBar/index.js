e5449ea1a429b465e7ecb57703b2f068b3df3632
nsy033 <nsy033@kaist.ac.kr>
2022-01-12 22:58:01 +0900

    Initialize react settings with d3

---
+++ src/elements/StackedBar/index.js
@@ -0,0 +1,12 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { select } from 'd3';
+
+function StackedBar() {
+    return (
+        <React.Fragment>
+            <h1>Show horizontal stacked bar</h1>
+        </React.Fragment>
+    );
+}
+
+export default StackedBar;
